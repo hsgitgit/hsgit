f322c0e884a90b1de6408f415b3fc5e8e8a93a0d
hsgitgit <59638089+hsgitgit@users.noreply.github.com>
2020-01-08 19:53:34 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 
 int main(void){
-	char c;
-	int i,j;
-	for(i=0;i<=10;i++){
-		for(j=0;j<=7;j++){
-			if(j%2==0){
-				printf("%2c",'A'+i);
-			}
-			else{
-				printf("%2d",j);
-			}
-			
+	int i,j,n,bSize,bCount;
+	int rb,cb;
+
+	printf("블록의 크기:");
+	scanf("%d",&bSize);
+	printf("블록의 개수: ");
+	scanf("%d",&bCount);
+
+	n=bSize * bCount;
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			rb=i/bSize;
+			cb=j/bSize;
+			if((rb+cb)%2==0)
+				printf("*");
+			else printf(" ");
 		}
 		printf("\n");
 	}
 }
+
+
