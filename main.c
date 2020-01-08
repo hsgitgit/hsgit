693cc42c0938d864d08da27e14e591f683b7753c
hsgitgit <59638089+hsgitgit@users.noreply.github.com>
2020-01-08 17:03:13 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-int main(){
+int main(void){
 	char c;
 	int i,j;
-	for(i=1;i<=5;i++){
-		for(c='a';c<='g';c++){
-			printf("%c",c);
-		}
-		printf("\n");
-		for(j=1;j<=7;j++){
-			printf("%d",j);
+	for(i=0;i<=10;i++){
+		for(j=0;j<=7;j++){
+			if(j%2==0){
+				printf("%2c",'A'+i);
+			}
+			else{
+				printf("%2d",j);
+			}
+			
 		}
 		printf("\n");
 	}
